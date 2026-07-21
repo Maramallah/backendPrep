44908790d9c2bf9a7c48c2c9a137ad5dc514a924
Maramallah <157317456+Maramallah@users.noreply.github.com>
2026-07-21 21:47:45 +0300

    [1071] [Time Beats: 94.36%] [Memory Beats: 27.64%] - LeetPush

---
+++ 1071-Greatest-Common-Divisor-of-Strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+         
+         if (!((str1+str2).equals(str2+str1))){
+            return "";
+         }
+         
+     
+         int len = gcd(str1.length(), str2.length());
+         String result = str1.substring(0, len);
+         return result;
+        
+    }
+      private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+}
