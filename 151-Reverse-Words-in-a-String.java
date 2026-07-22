3fac00462ca49f24e18cf5d8927b7b82296e57f7
Maramallah <157317456+Maramallah@users.noreply.github.com>
2026-07-22 12:12:18 +0300

    [151] [Time Beats: 99.04%] [Memory Beats: 87.31%] - LeetPush

--- 151-Reverse-Words-in-a-String.java
+++ 151-Reverse-Words-in-a-String.java
@@ -1,16 +1,34 @@
 class Solution {
-   public String reverseWords(String s) {
-    String[] words = s.trim().split("\\s+");
+    public String reverseWords(String s) {
 
-    StringBuilder ans = new StringBuilder();
+        StringBuilder x = new StringBuilder();
+        s = s.trim();
 
-    for (int i = words.length - 1; i >= 0; i--) {
-        ans.append(words[i]);
+        int end = s.length();
 
-        if (i > 0)
-            ans.append(" ");
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            if (s.charAt(i) == ' ') {
+
+                
+                if (i == end - 1) {
+                    continue;
+                }
+
+                x.append(s.substring(i + 1, end));
+                x.append(" ");
+
+                
+                while (i >= 0 && s.charAt(i) == ' ') {
+                    i--;
+                }
+
+                end = i + 1;
+            }
+        }
+
+        x.append(s.substring(0, end));
+
+        return x.toString();
     }
-
-    return ans.toString();
-}
 }
