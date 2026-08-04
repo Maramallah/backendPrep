526e50a0a440d02fec255d5ae112f28d20ae5c65
Maramallah <157317456+Maramallah@users.noreply.github.com>
2026-08-04 10:31:55 +0300

    [197] [Time Beats: 94.91%] - LeetPush

---
+++ 197-Rising-Temperature.sql
@@ -0,0 +1,11 @@
+SELECT id 
+FROM (
+    SELECT
+        id,
+        recordDate,
+        temperature,
+        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp
+    FROM Weather
+) t
+WHERE recordDate = DATE_ADD(prev_date, INTERVAL 1 DAY) and temperature>prev_temp
