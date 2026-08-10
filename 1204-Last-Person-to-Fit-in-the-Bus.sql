6e72299e1fdc8d89aa185a1df20e301ee25f2ff0
Maramallah <157317456+Maramallah@users.noreply.github.com>
2026-08-10 10:47:33 +0300

    [1204] [Time Beats: 45.77%] - LeetPush

--- 1204-Last-Person-to-Fit-in-the-Bus.sql
+++ 1204-Last-Person-to-Fit-in-the-Bus.sql
@@ -1,17 +1,8 @@
 # Write your MySQL query statement below
-WITH CumulativeWeight AS (
-    SELECT 
-        person_name,
-        SUM(weight) OVER(ORDER BY turn ASC) AS Total_Weight
-    FROM 
-        Queue
-)
-SELECT 
-    person_name
-FROM 
-    CumulativeWeight
-WHERE 
-    Total_Weight <= 1000
-ORDER BY 
-    Total_Weight DESC
-LIMIT 1;
+select person_name from (
+select person_name , 
+sum(weight) over (order by turn) as TotalWeight
+from Queue) t
+where TotalWeight<= 1000
+order by TotalWeight desc
+limit 1
